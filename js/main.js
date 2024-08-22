9b69c734413ef4611fda1cc17b6e96c2be773b97
Xiang511 <test@gmail.com>
2024-08-22 08:27:19 +0800

    feat: 簡單列出所有地點

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ let name = document.querySelector('.name');
         fetch("https://fhy.wra.gov.tw/Api/v2/Basic/Basin?$top=15")
         .then(response => response.json())
         .then(data => {
-            console.log(data.Data[0].Name.zh_TW);
-            name.innerHTML = data.Data[0].Name.zh_TW;
+            console.log(data);
+            data.Data.forEach(item => {       
+                name.innerHTML += item.Name.zh_TW + '<br>';
+                console.log(item.Name.zh_TW);
+            });
         });
