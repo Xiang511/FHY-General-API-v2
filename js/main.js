7f7674be959423b3d593081c030ac5422ec73b7c
Xiang511 <toby91511@gmail.com>
2024-08-24 16:03:40 +0800

    feat: basic layout

--- js/main.js
+++ js/main.js
@@ -6,45 +6,39 @@ for (var i = 0; i < menuBtn.length; i++) {
     });
 }
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const nav = document.querySelector('.main-header');
     const bg_div = document.querySelector('.bg');
     const isScrolled = window.scrollY > 50;
     nav.classList.toggle('nav-scrolled', isScrolled);
     nav.classList.toggle('nav-default', !isScrolled);
 
-}); 
+});
 
 
 
 let name = document.querySelector('.name');
 let citys = document.querySelector('.city');
-
-        fetch("https://fhy.wra.gov.tw/Api/v2/Basic/Basin?$top=15")
-        .then(response => response.json())
-        .then(data => {
-            // console.log(data);
-            data.Data.forEach(item => {       
-                name.innerHTML += item.Name.zh_TW + '<br>';
-                // console.log(item.Name.zh_TW);
-            });
-        });
+let container = document.querySelector('.container');
+let Updatetime = document.querySelector('.Updatetime');
 
 
-        let city = []
 
-        async function GetCity(){
-            data  = await fetch("https://fhy.wra.gov.tw/Api/v2/Basic/City?$top=22").then(response => response.json());  
-           console.log(data);
-           data.Data.forEach(item => {
-                city.push({
-                     id: item.Code,
-                     name: item.Name.zh_TW
-           })
-            citys.innerHTML += item.Name.zh_TW + '<br>';
-        }
-        )
-        }
-        
-        GetCity()
-        console.log(city);
+
+
+async function GetCity() {
+    data = await fetch("https://fhy.wra.gov.tw/Api/v2/Basic/City?$top=23").then(response => response.json());
+    console.log(data);
+    Updatetime.textContent += data.UpdataTime;
+    data.Data.forEach(item => {
+        let cityDiv = document.createElement('div');
+        cityDiv.textContent = item.Name.zh_TW;
+        container.appendChild(cityDiv);
+
+        cityDiv.classList.add('grid-item');
+    })
+
+}
+
+
+GetCity()
